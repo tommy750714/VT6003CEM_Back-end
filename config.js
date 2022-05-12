97c6cf63fe7834b1cfe185a0a5b74e385f6640fd
Yeung69420 <97737189+tommy750714@users.noreply.github.com>
2022-05-12 19:19:21 +0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  host: "satao.db.elephantsql.com",
-  port: 5432,
-  user: "kpaoilgq",
-  password: "ZXkqhsVtuqgXFAf4AZriDQF5biYoS0fV",
-  database: "kpaoilgq",
-  connection_limit: 100
-}
